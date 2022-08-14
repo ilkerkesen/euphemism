ace208ad037eb4b28a059c77bb47ec0a52272aab
İlker Kesen <ilkerksn@gmail.com>
2022-08-15 02:45:04 +0300

    implement deberta baseline (#1)

---
+++ euphemism/util.py
@@ -0,0 +1,45 @@
+import os
+import os.path as osp
+
+import pytorch_lightning as pl
+
+
+def create_callbacks(config, log_dir):
+    checkpoints_path = osp.join(log_dir, 'checkpoints')
+    config['checkpoint']['dirpath'] = checkpoints_path
+    checkpoint_callback = pl.callbacks.ModelCheckpoint(**config['checkpoint'])
+    last_ckpt = osp.join(checkpoints_path, 'last.ckpt')
+    last_ckpt = last_ckpt if osp.isfile(last_ckpt) else None
+    ckpt_path = config['trainer']['resume_from_checkpoint']
+
+    if last_ckpt is not None and ckpt_path is not None:
+        raise Exception('resume checkpoint passed (last.ckpt exists already)')
+
+    ckpt_path = last_ckpt if ckpt_path is None else ckpt_path
+    if ckpt_path is not None and not osp.isfile(ckpt_path):
+        raise Exception('ckpt does not exist at {}'.format(ckpt_path))
+
+    return [checkpoint_callback], ckpt_path
+
+
+def create_logger(config):
+    assert config['logger'].get('version') is not None
+    if config['logger']['version'] == 'debug':
+        return None
+    config['logger']['save_dir'] = osp.abspath(
+        osp.expanduser(config['logger']['save_dir']))
+    if config['logger']['name'] is None:
+        model_config = config.get('model', {})
+        architecture = model_config.get('name', 'SomeModel')
+        config['logger']['name'] = f'{architecture}'
+    logger = pl.loggers.TensorBoardLogger(**config['logger'])
+    return logger
+
+
+def process_config(config):
+    model_config = config.get('model', {})
+    text_encoder = model_config.get('text_encoder', 'microsoft/deberta-base')
+    data_config = config.get('data', {})
+    data_config['tokenizer'] = text_encoder
+    config['data'] = data_config
+    return config
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+
+setup(
+    name="euphemism",
+    version="0.0.1",
+    description="FigLang2022 @ EMNLP, Shared Task on Euphemism Detection",
+    url="https://github.com/ilkerkesen/euphemism",
+    license="MIT",
+    packages=["euphemism"],
+    zip_safe=False,
+    include_package_data=True,
+)
